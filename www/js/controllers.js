856873c7e51861ee4eaa4fa48ef2d9046de7dfe5
Joey wang <wxluckly@gmail.com>
2016-02-26 12:00:31 +0800

    coding

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,25 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngResource'])
 
-.controller('WelcomeCtrl', function($scope) {
+.controller('WelcomeCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get("http://smjjl.dev/wap_api/bargains/welcome?per=10").success(function(data){
+    $scope.items = data.bargains;
+    console.log(data.bargains)
+  }).error(function(){
+    alert('信息获取失败');
+  });
 
-  
-})
+  $scope.doRefresh = function() {
+    $http.get("http://smjjl.dev/wap_api/bargains/welcome?per=10").success(function(data){
+      data.bargains.forEach(function(element, index, array){
+        $scope.items.unshift(element);
+      });
+      $scope.$broadcast("scroll.refreshComplete");
+    }).error(function(){
+      alert('信息获取失败');
+    });
+    
+  };
+
+}])
 
 .controller('BargainCtrl', function($scope) {})
