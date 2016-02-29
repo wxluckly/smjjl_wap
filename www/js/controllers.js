008e613b8675e843c575893a73474e4fd47e06d7
Joey wang <wxluckly@gmail.com>
2016-02-29 17:03:10 +0800

    coding

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -55,7 +55,7 @@ angular.module('starter.controllers', ['ngResource'])
   $http.get(Settings.api_domain + "/wap_api/bargains/rank").success(function(data){
     $scope.items = data.bargains;
   }).error(function(){
-    alert('信息获取失败');
+    alert(Settings.api_domain + '信息获取失败');
   });
 
 }])
@@ -64,6 +64,6 @@ angular.module('starter.controllers', ['ngResource'])
   $http.get(Settings.api_domain + "/wap_api/products/show?id=" + $stateParams.productId).success(function(data){
     $scope.product = data;
   }).error(function(){
-    alert('信息获取失败');
+    alert(Settings.api_domain + '信息获取失败');
   });
 }])
