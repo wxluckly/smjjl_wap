57c8fa6f1b6969285f1b1d5dc1209d6dc2b4ce00
Joey wang <wxluckly@gmail.com>
2016-02-29 16:51:44 +0800

    coding

--- www/js/services.js
+++ www/js/services.js
@@ -3,6 +3,7 @@ angular.module('starter.services', [])
 .factory('Settings', function() {
   // Might use a resource here that returns a JSON array
   return {
-    api_domain: 'http://192.168.2.218'
+    api_domain: 'http://smjjl.com'
+    // api_domain: 'http://192.168.2.218'
   };
 });
